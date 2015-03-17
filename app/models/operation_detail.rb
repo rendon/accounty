e70e0b0c5b6b2a0b55e3a104b37490c28f7729aa
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2015-03-17 12:07:12 -0600

    Fix bug with operation details.
    The OperationDetail model was accepting account_id and operation_id with nil
    values.

--- app/models/operation_detail.rb
+++ app/models/operation_detail.rb
@@ -3,4 +3,6 @@ class OperationDetail < ActiveRecord::Base
   belongs_to :operation
   belongs_to :account
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :account_id, presence: true
+  validates :operation_id, presence: true
 end
